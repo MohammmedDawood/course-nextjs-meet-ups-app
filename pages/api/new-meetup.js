27cafc5db43343d52c0c6dd0b2f8269238b02491
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-26 04:44:31 +0200

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,26 @@
+import { MongoClient } from "mongodb";
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
     //store in a database
+    const client = await MongoClient.connect(
+      "mongodb+srv://mohammeddawood:test1234@cluster0.iidfcts.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
