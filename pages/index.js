71ff80bc56f32327f0fe30339615898296881f33
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-26 05:00:07 +0200

    Getting Data From The Database

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,27 @@
-import React, { useEffect, useState } from "react";
+import { MongoClient } from "mongodb";
+import React from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 5, 12345 Some City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Some address 10, 12345 Some City",
-    description:
-      "This is a second, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "This is a first meetup",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+//     address: "Some address 5, 12345 Some City",
+//     description:
+//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//   },
+//   {
+//     id: "m2",
+//     title: "This is a second meetup",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+//     address: "Some address 10, 12345 Some City",
+//     description:
+//       "This is a second, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//   },
+// ];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -29,9 +30,28 @@ function HomePage(props) {
 //static generation - pre-rendering SSG
 export async function getStaticProps() {
   // fetch data from an API
+  //get data from a database
+  const client = await MongoClient.connect(
+    "mongodb+srv://mohammeddawood:test1234@cluster0.iidfcts.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  console.log(meetups);
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1, // regenerate the page every 1 seconds
   };
