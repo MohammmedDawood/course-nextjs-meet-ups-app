7141c12804c868f02afb3cee8fdfd9e772616f7c
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-26 04:06:20 +0200

    Working with Params for SSG Data Fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
@@ -10,6 +10,27 @@ function MeetupDetails() {
       description="This is a first meetup"
     />
   );
+}
+
+//static generation - pre-rendering SSG
+export async function getStaticProps(context) {
+  console.log(context);
+  // fetch data from an API
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A first meetup",
+        address: "Meetupstreet 5, 12345 Meetup City",
+        description: "This is a first meetup",
+      },
+    },
+    revalidate: 1, // regenerate the page every 1 seconds
+  };
 }
 
 export default MeetupDetails;
